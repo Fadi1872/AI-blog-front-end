b32b14fa6b00c5b1886027e582797854d5ccbe66
Freddy18 <fadinoumih18@gmail.com>
2024-01-18 03:28:30 +0300

    FeaturedVideos-2

--- src/components/CustomVideo/CustomVideo.jsx
+++ src/components/CustomVideo/CustomVideo.jsx
@@ -5,22 +5,26 @@ import play from './../../assets/icons/play.svg'
 function CustomVideo({ video }) {
 
     const [watched, setwatched] = useState(false);
-    const vid = useRef(0);
+    const [duration, setdurationV] = useState('00:00');
+    const vid = useRef(null);
 
     const handleplay = () => {
         setwatched(true);
         vid.current.play();
     }
 
-    let vidDuration = Number(vid.current.duration);
-    let duration = `${Math.floor(vidDuration/60)}:${vidDuration%60 < 10 ? `0${Math.floor(vidDuration%60)}` :Math.floor(vidDuration%60)}`
+    const handleVideo = () => {
+        let vidDuration = Number(vid.current.duration);
+        setdurationV(`${Math.floor(vidDuration/60)}:${vidDuration%60 < 10 ? `0${Math.floor(vidDuration%60)}` :Math.floor(vidDuration%60)}`)
+        console.log(duration);
+    }
 
     return ( 
         <div className='fa_CustomVideo position-relative'>
-            <video src={video} controls={watched} ref={vid}>user video</video>
+            <video src={video} controls={watched} ref={vid} onLoadedMetadata={handleVideo}>user video</video>
             <div className={`fa_customVideo-play_button ${watched && 'd-none'}`}>
-                <img src={play} alt="play icon" onClick={handleplay} className='cursor' />
-                <p className='mb-0 sub_title-125 text-gray-50'>{duration}min</p>
+                <img src={play} alt="play icon" onClick={handleplay} className='cursor fa_play-video_icon' />
+                <p className='mb-0 sub_title-125 text-gray-50'>{duration} min</p>
             </div>
         </div>
     )
