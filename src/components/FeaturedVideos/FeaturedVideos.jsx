c085bcc964b250edff97ea79a82af7f511c86940
Freddy18 <fadinoumih18@gmail.com>
2024-01-18 03:35:50 +0300

    FeaturedVideos-3

--- src/components/FeaturedVideos/FeaturedVideos.jsx
+++ src/components/FeaturedVideos/FeaturedVideos.jsx
@@ -4,6 +4,7 @@ import video from './../../assets/videos/starship.mp4'
 import video2 from './../../assets/videos/vid2.mp4'
 import { Row } from 'react-bootstrap'
 import SectionTitle_Button from '../SectionTitle_button/SectionTitle_Button'
+import FeaturedVid from '../../data/FeatturedVid'
 
 function FeaturedVideos() {
     return (
@@ -12,22 +13,15 @@ function FeaturedVideos() {
             <div className='fa_Featured-videos bg-dark-08 overflow-hidden'>
                 <div className='container_custom' >
                     <Row className='fa_featured-videos_grid'>
-                        <FeaturedVideoCard
-                            title='Mars Exploration: Unveiling Alien Landscapes'
-                            subtitle={`Embark on a journey through the Red Planet's breathtaking landscapes and uncover the mysteries of Mars.`}
-                            video={video} />
-                        <FeaturedVideoCard
-                            title='Blockchain Explained: A Revolution in Finance'
-                            subtitle='Delve into the world of blockchain technology and its transformative impact on the financial industry.'
-                            video={video2} />
-                        <FeaturedVideoCard
-                            title='Breaking the Silence: Mental Health Awareness in the Workplace'
-                            subtitle='An exploration of the importance of mental health awareness and the initiatives reshaping workplaces for employee well-being.'
-                            video={video} />
-                        <FeaturedVideoCard
-                            title='Revolutionizing Investment Strategies'
-                            subtitle='An in-depth look at global efforts to conserve biodiversity and safeguard endangered species from extinction.'
-                            video={video2} />
+                        {
+                            FeaturedVid.map(vid => (
+                                <FeaturedVideoCard
+                                    key={vid.id}
+                                    title={vid.title}
+                                    subtitle={vid.subtitle}
+                                    video={vid.vid} />
+                            ))
+                        }
                     </Row>
                 </div>
             </div>
