e5ec575272a2c78f1a483bb489d5b3ecb2925c10
HindAljendy <isonhindjendy1998@gmail.com>
2024-01-21 17:05:46 +0300

    FutureTechFeatures

---
+++ src/components/FutureTechFeaturesNews/FutureTechFeaturesNews.jsx
@@ -0,0 +1,20 @@
+import './FutureTechFeaturesNews.css'
+
+const FutureTechFeaturesNews = ({ img, title, discription , className }) => {
+  return (
+    <div className="HJ_FutureTechFeaturesNews">
+      <div>
+        <img src={img} alt="icon"  className={className}/>
+      </div>
+      <div>
+        <h5 className="title-2 text-absolute-White ">{title}</h5>
+        <p className=" sub_title-125_feature text-gray-60 ">{discription}</p>
+      </div>
+      
+
+     
+    </div>
+  );
+};
+
+export default FutureTechFeaturesNews;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
 import ReelWords from '../components/ReelWords/ReelWords'
+import FutureTechFeatures from '../components/FutureTechFeatures/FutureTechFeatures'
 
 function Home() {
   return (
     <>
       <Hero />
+      <FutureTechFeatures/>
       <ReelWords />
     </>
   )
