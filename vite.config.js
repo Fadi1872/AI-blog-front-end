82ad5a87ac1e16b79bf2fcb83ada21b6453c1ed9
Freddy18 <fadinoumih18@gmail.com>
2024-01-29 15:15:17 +0300

    add: deploy-4

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/AI-blog-front-end",
   plugins: [react()],
-  base: "/AI-blog-front-end/"
 })
