13434add82185229485f97199ff43619a97ad084
HindAljendy <isonhindjendy1998@gmail.com>
2024-01-30 22:53:48 +0300

    proplem in Prodcast Card

--- src/pages/Podcasts.jsx
+++ src/pages/Podcasts.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PageTitle from '../components/PageTitle/PageTitle'
 import PodcastEpisodes from '../components/PodcastEpisodes/PodcastEpisodes'
+import ProdcastSectionCards from '../components/ProdcastSectionCards/ProdcastSectionCards'
 
 function Podcasts() {
   return (
@@ -12,6 +13,7 @@ function Podcasts() {
         title4="through"
         summary="Dive deep into the AI universe with our collection of insightful podcasts. Explore the latest trends, breakthroughs, and discussions on artificial intelligence. Whether you're an enthusiast or a professional, our AI podcasts offer a gateway to knowledge and innovation." />
         <PodcastEpisodes />
+        < ProdcastSectionCards/>
     </div>
   )
 }
