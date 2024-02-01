d2ba910e7978fc2d324705de5562e83a0aff8097
HindAljendy <isonhindjendy1998@gmail.com>
2024-02-01 05:01:22 +0300

    Committing changes before switching branches

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PageTitle from '../components/PageTitle/PageTitle'
 import DepthReportsAnalysis from '../components/DepthReportsAnalysis/DepthReportsAnalysis'
+import ReportCardsSection from '../components/ReportCardsSection/ReportCardsSection'
 
 function Resources() {
   return (
@@ -10,6 +11,8 @@ function Resources() {
         title2="Knowledge"
         summary="Dive deep into the AI universe with our collection of insightful podcasts. Explore the latest trends, breakthroughs, and discussions on artificial intelligence. Whether you're an enthusiast or a professional, our AI podcasts offer a gateway to knowledge and innovation." />
       <DepthReportsAnalysis />
+      <ReportCardsSection/>
+      
     </div>
   )
 }
